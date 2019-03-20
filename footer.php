6643640ac499f9526b19feaa0abe6b7b82559417
Jacob-onduro <47317135+Jacob-onduro@users.noreply.github.com>
2019-03-20 11:53:02 +0300

    Add files via upload

---
+++ footer.php
@@ -0,0 +1,17 @@
+<div class="well" style="background-color: green;text-align: center;"id="footer">
+<?php
+
+echo "<a href=\"javascript:history.go(-1)\"class='pull-left'style='position:relative;top:-10px;color:white;'><span class='glyphicon glyphicon-chevron-left'></span> Back</a>";
+
+?>
+	<ul style="margin: -15px;">
+		<li><a href="#">Terms & Conditions</a></li>
+		<li><a href="#"> Designed by Jacob Onduro</a></li>
+		
+	</ul>
+</div><!--End of well-->
+	<script type="text/javascript"src="jquery-1.11.1.min.js"></script>	
+	<script type="text/javascript"src="js/bootstrap.min.js"></script>
+	<script type="text/javascript"src="script.js"></script>
+	</body>
+</html>
---
+++ header.php
@@ -0,0 +1,22 @@
+<?php
+session_start();
+?>
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>Eshamba</title>
+		<meta charset="utf-8"/>
+		<meta name="viewport"content="width=device-width,initial-scale=1.0"/>
+		<link type="text/css"rel="stylesheet"href="css/bootstrap.min.css"/>
+		<link type="text/css"rel="stylesheet"href="w3.css"/>
+		<link type="text/css"rel="stylesheet"href="style.css"/>
+		<style media="printer">
+	.noprint*{
+		display: none;
+	}
+</style>
+		
+	</head>
+	<body>
+		
+	
